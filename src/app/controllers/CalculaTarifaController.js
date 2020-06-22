0e53fd8a9c5329686da90b5a66cba5bff976882c
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-06-22 16:54:26 -0400

    finalizando a parte logica que calcula as tarifas

---
+++ src/app/controllers/CalculaTarifaController.js
@@ -0,0 +1,54 @@
+module.exports = {
+  calculaTarifaPage: (req, res) => {
+    const { origem, destino, tempo, plano } = req.body;
+    let tarifa = calculaTarifa(origem, destino);
+    let execende = minutosExcedentes(Number(plano), Number(tempo));
+    const valorSemPlano = tempo * tarifa;
+    const valorComJuros = ((execende * tarifa) / 100) * 10;
+    const valorComPlano = valorComJuros + execende * tarifa;
+
+    return res.json({
+      valorComPlano,
+      valorSemPlano,
+    });
+  },
+};
+
+function calculaTarifa(origem, destino) {
+  let tarifa = 0;
+  if (origem === "011" && destino === "016") {
+    return (tarifa = 1.9);
+  }
+  if (origem === "016" && destino === "011") {
+    return (tarifa = 2.9);
+  }
+  if (origem === "011" && destino === "017") {
+    return (tarifa = 1.7);
+  }
+  if (origem === "017" && destino === "011") {
+    return (tarifa = 2.7);
+  }
+  if (origem === "011" && destino === "018") {
+    return (tarifa = 0.9);
+  }
+  if (origem === "018" && destino === "011") {
+    return (tarifa = 1.9);
+  }
+
+  return tarifa;
+}
+
+function minutosExcedentes(plano, tempo) {
+  let execende = 0;
+  if (plano === 30 && tempo > 30) {
+    return (execende = tempo - plano);
+  }
+  if (plano === 60 && tempo > 60) {
+    return (execende = tempo - plano);
+  }
+  if (plano === 120 && tempo > 120) {
+    return (execende = tempo - plano);
+  }
+
+  return execende;
+}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const {Router} = require('express')
 const routes = Router()
+const {calculaTarifaPage} = require('./app/controllers/CalculaTarifaController')
 
 routes.get("/",(req, res)=>{
   res.json({message: "Bem vindo!"})
 })
+
+routes.post('/calcula-tarifa', calculaTarifaPage)
 
 module.exports = routes
